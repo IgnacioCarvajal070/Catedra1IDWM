0ea97bc33fe7cdf00c163357967ce0d569f82301
IgnacioCarvajal070 <130288880+IgnacioCarvajal070@users.noreply.github.com>
2024-10-02 19:51:45 -0300

    feat: model user created

---
+++ catedra/src/Models/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace catedra.src.Models
+{
+      public class User
+    {
+        public string rut {get;set; }
+        public string nombre {get;set; }
+        public string correo {get;set; }
+
+        public string genero {get;set; }
+
+        public string fecha {get;set; }
+    }
+}
